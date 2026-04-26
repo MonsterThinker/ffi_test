5060feb67b33120c215f3508e8fb844e14d3983c
MonsterThinker <0uh3264411v084k@gmail.com>
2026-04-27 02:13:31 +0900

    ヘッダファイル修正

--- include/rustlib.h
+++ include/rustlib.h
@@ -1,7 +1,7 @@
 #ifndef RUSTLIB_H
 #define RUSTLIB_H
 
-const char* hello_from_rust(const char* input);
-Const void free_string(const char* ptr);
+const char* hello_from_rust(const char* input, const char* dbpath);
+const void free_string(const char* ptr);
 
 #endif
